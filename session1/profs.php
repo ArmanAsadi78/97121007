eb28c7c6b3344a89e5ae0449afceb33707f5b070
ArmanAsadi78 <arman.asadi2019pc@gmail.com>
2023-11-17 20:24:09 +0330

    session 2 after session 1

---
+++ session1/profs.php
@@ -0,0 +1,12 @@
+<?php
+$profs = [
+    'prof1' => [
+        'name' => 'Nasser Akhoundi',
+    ],
+    'prof2' => [
+        'name' => 'Reza Saneyan'
+    ],
+    'prof3' => [
+        'name' => 'Saeed Najd'
+    ],
+];
